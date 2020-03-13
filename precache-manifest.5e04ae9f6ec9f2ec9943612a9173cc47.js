f1cfc7d797660ac7bca932a58c2d3afcc7923adf
Marcin Moryc <m.moryc@protonmail.com>
2020-03-13 19:16:23 +0100

    Updates

---
+++ precache-manifest.5e04ae9f6ec9f2ec9943612a9173cc47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ab663082370f4f07d0ff3d9d5cf9656",
+    "url": "/ToDo/index.html"
+  },
+  {
+    "revision": "096465a8283af7c26c1e",
+    "url": "/ToDo/static/css/main.a7fb0def.chunk.css"
+  },
+  {
+    "revision": "5ab1264385c0f3f0ed55",
+    "url": "/ToDo/static/js/2.38b9ab90.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/ToDo/static/js/2.38b9ab90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "096465a8283af7c26c1e",
+    "url": "/ToDo/static/js/main.958ef0e5.chunk.js"
+  },
+  {
+    "revision": "ffbd84bb8a08fc036b0e",
+    "url": "/ToDo/static/js/runtime-main.a518d0df.js"
+  }
+]);
